ea5211defee17cbfbd26cd4e71f112e3b610ea1f
Nithish <kumarnithish6112002@gmail.com>
2023-09-10 11:14:48 +0530

    working with front-end

--- api/main.py
+++ api/main.py
@@ -5,9 +5,18 @@ import numpy as np
 from io import BytesIO
 from PIL import Image
 import tensorflow as tf
+from fastapi.responses import FileResponse
+from fastapi.templating import Jinja2Templates
+import os
+
 
 app = FastAPI()
+'''templates = Jinja2Templates(directory="frontend_build")'''
 
+'''@app.get("/", include_in_schema=False)
+async def serve_frontend():
+    # Serve the main HTML file
+    return templates.TemplateResponse("index.html", {"request": None})'''
 origins = [
     "http://localhost",
     "http://localhost:3000",
@@ -24,21 +33,24 @@ MODEL = tf.keras.models.load_model("../perfect_ models/1")
 
 CLASS_NAMES = ["Early Blight", "Late Blight", "Healthy"]
 
+
 @app.get("/ping")
 async def ping():
     return "Hello"
+
 
 def read_file_as_image(data) -> np.ndarray:
     image = np.array(Image.open(BytesIO(data)))
     return image
 
+
 @app.post("/predict")
 async def predict(
-    file: UploadFile = File(...)
+        file: UploadFile = File(...)
 ):
     image = read_file_as_image(await file.read())
     img_batch = np.expand_dims(image, 0)
-    
+
     predictions = MODEL.predict(img_batch)
 
     predicted_class = CLASS_NAMES[np.argmax(predictions[0])]
@@ -47,6 +59,7 @@ async def predict(
         'class': predicted_class,
         'confidence': float(confidence)
     }
+
 
 if __name__ == "__main__":
     uvicorn.run(app, host='localhost', port=8000)
