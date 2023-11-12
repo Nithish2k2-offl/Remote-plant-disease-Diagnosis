19fd6e8b8296784e7af2d6dcc13e3d24f7e8faa5
Nithish <kumarnithish6112002@gmail.com>
2023-11-12 19:17:58 +0530

    Basic change

--- RPD/frontend/src/components/PredictForm.js
+++ RPD/frontend/src/components/PredictForm.js
@@ -4,22 +4,12 @@ import React, { useState } from 'react';
 function PredictForm() {
   const [file, setFile] = useState(null);
   const [result, setResult] = useState(null);
-  const [error, setError] = useState(null);
 
   const handleFileChange = (event) => {
     setFile(event.target.files[0]);
   };
 
   const handlePredict = async () => {
-    // Check if no file is chosen
-    if (!file) {
-      setError('Please choose an image file before predicting.');
-      return;
-    }
-
-    // Clear previous error
-    setError(null);
-
     const formData = new FormData();
     formData.append('file', file);
 
@@ -33,8 +23,7 @@ function PredictForm() {
         const data = await response.json();
         setResult(data);
       } else {
-        setResult(null); // Clear previous result
-        setError('Prediction failed');
+        setResult({ error: 'Prediction failed' });
       }
     } catch (error) {
       console.error('Error:', error);
@@ -45,7 +34,6 @@ function PredictForm() {
     <div>
       <input type="file" accept="image/*" onChange={handleFileChange} />
       <button onClick={handlePredict}>Predict</button>
-      {error && <p style={{ color: 'red' }}>{error}</p>}
       {result && (
         <div>
           <p>Class: {result.class}</p>
